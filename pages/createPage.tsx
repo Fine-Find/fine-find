f7b0dc1f82f622bef194ea4bbd8d627282f156a6
blainehoyt <blainehoyt@gmail.com>
2021-02-24 20:59:57 -0600

    insta/shopify functionality

---
+++ pages/createPage.tsx
@@ -0,0 +1,60 @@
+import { withRouter } from "next/router";
+import styles from "../styles/Home.module.css";
+import Head from "next/head";
+
+const createPageEndpoint = "/api/createPage";
+
+const createPage = async (body) => {
+    const response = await fetch(`${createPageEndpoint}`, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+        body: JSON.stringify(body),
+    });
+    return await response.json();
+};
+
+const fetcher = (url, body) =>
+    fetch(url, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            body,
+        }),
+    }).then((res) => res.json());
+
+function About({ router: { query } }) {
+    const object = JSON.parse(query.object);
+
+    async function handleClick(e) {
+        e.preventDefault();
+        console.log("whoopie");
+        let link: string = await createPage({ imageUrl: object });
+
+        return link;
+        // const { data, error } = useSwr("/api/createPage", fetcher("test"));
+    }
+
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Create Link</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <h1>Your Post</h1>
+            <img src={object} />
+
+            <button
+                type="button"
+                className="inline-flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-50 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo active:bg-indigo-200 transition ease-in-out duration-150"
+                onClick={handleClick}
+            >
+                Create Link
+            </button>
+        </div>
+    );
+}
+
+export default withRouter(About);
---
+++ pages/instagram.tsx
@@ -0,0 +1,57 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Instagram from "instagram-web-api";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Home({ posts }) {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Instagram Posts</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <h1>Instagram Posts</h1>
+            <div className={styles.grid}>
+                {posts.map(({ node }, i) => {
+                    return (
+                        <Link
+                            href={{
+                                pathname: "/createPage",
+                                query: {
+                                    object: JSON.stringify(node.display_resources[0].src),
+                                },
+                            }}
+                        >
+                            <a>
+                                <div className={styles.card}>
+                                    <img src={node.thumbnail_resources[1].src} />
+                                    {/*<p>{node.edge_media_to_caption.edges[0]?.node.text}</p>*/}
+                                </div>
+                            </a>
+                        </Link>
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
+
+export async function getStaticProps(context) {
+    const client = new Instagram({
+        username: "***",
+        password: "***",
+    });
+    await client.login();
+
+    const response = await client.getPhotosByUsername({
+        username: "thefinefind",
+    });
+
+    return {
+        props: {
+            posts: response.user.edge_owner_to_timeline_media.edges,
+        }, // will be passed to the page component as props
+    };
+}
