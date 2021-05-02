f7d76e50405d6c86d402bc5d10931ceb44016223
Ben Gooding <bcgooding@gmail.com>
2021-05-02 10:31:50 -0500

    Dashboard | Add hover to grid

--- src/components/MasonGridList/MasonGridList.styles.ts
+++ src/components/MasonGridList/MasonGridList.styles.ts
@@ -2,6 +2,7 @@ import makeStyles from '@material-ui/core/styles/makeStyles';
 
 export const useStyles = makeStyles((theme) => ({
   gridList: {
+    overflow: 'visible',
   },
   loading: {
     textAlign: 'center',
@@ -9,24 +10,12 @@ export const useStyles = makeStyles((theme) => ({
   masonic: {
     padding: '8px',
     width: '100%',
-    maxWidth: '960px',
+    //maxWidth: '960px',
     margin: '0 auto 0',
     boxSizing: 'border-box',
   },
   card: {
-    display: 'flex',
-    // flexDirection: 'column',
-    //background: '#1d2326',
-    //borderRadius: '1rem',
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    transition: 'transform 100ms ease-in-out',
-    // width: '100%',
-    // minHeight: '100px',
-    'span:last-of-type': {
-      color: '#fff',
-      padding: '0.5rem',
-    },
+    transition: 'transform 300ms ease-in-out',
     '&:hover': {
       position: 'relative',
       background: '#f2fafe',
@@ -34,16 +23,11 @@ export const useStyles = makeStyles((theme) => ({
       zIndex: '1000',
       boxShadow:
         '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
-      'span:last-of-type': {
-        color: '#1d2326',
-        padding: '0.5rem',
-      },
     },
   },
   img: {
     width: '100%',
     display: 'block',
-    //borderRadius: '0.5rem',
   },
   container: {
     minHeight: '100vh',
