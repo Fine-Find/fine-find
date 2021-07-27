cba45bf9cdfc4a9daf7caf3b4fd0ac61a92b8e66
hanhuyeny2k <hanhuyeny2k@gmail.com>
2021-07-26 21:31:51 -0500

    button with adjust color, size, label and onclick action

---
+++ src/components/shared/OutlineButton/OutlineButton.stories.tsx
@@ -0,0 +1,22 @@
+import {Button} from '.';
+
+
+export default {
+  title: 'Component/Button',
+  component: Button,
+  argTypes: {
+    label: { control: 'text' },
+    textColor: { control: 'color'},
+    borderColor: { control: 'color'},
+    backgroundColor: { control: 'color'},
+  },
+};
+
+const Template = args => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Submit'
+};
+
+
---
+++ src/components/shared/OutlineButton/index.tsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import { action } from "@storybook/addon-actions";
+
+export const Button = ({
+  
+  label,
+  borderWidth = 'medium',
+  cellPadding = 'medium',
+  textColor = 'white',
+  borderColor = '#494866',
+  backgroundColor = '#a6a5b8',
+}) => {
+
+  const cellPaddingMap = {
+    small: '3px',
+    medium: '8px',
+    large: '15px',
+  };
+  const borderWidthMap = {
+    thin: '1px',
+    medium: '2px',
+    thick: '3px',
+  };
+
+  return (
+    <button type='button' onClick={action('clicked')}
+          style={{
+            color: textColor,
+            padding: cellPaddingMap[cellPadding],
+            backgroundColor: backgroundColor,
+            border: `${borderWidthMap[borderWidth]} solid ${borderColor}`,
+            borderRadius: 100,
+          }}
+        >
+          {label}
+        </button>
+  );
+};
+
+Button.propTypes = {
+  textColor: PropTypes.string,
+  borderColor: PropTypes.string,
+  backgroundColor: PropTypes.string,
+  borderWidth: PropTypes.oneOf(['thin', 'medium', 'thick']),
+  cellPadding: PropTypes.oneOf(['small', 'medium', 'large']),
+  label: PropTypes.string,
+};
+
+
